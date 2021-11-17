c71ac39946db8c2e011d36d47e39def624d94499
Perez Etel S <38335096+EtelS@users.noreply.github.com>
2021-11-17 01:21:43 -0300

    renderizado condicional

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './components/Header';
-import React from 'react';
 import StartGameScreen from './screens/StartGameScreen';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+  const [confirmedNumber, setconfirmedNumber] = useState();
+  const screen = confirmedNumber 
+  ? <Text>Game</Text>
+  : <StartGameScreen onStarGame={setconfirmedNumber} />
   return (
     <View style={styles.container}>
       <Header title="Adivina el numero" />
-      <StartGameScreen />
+      {screen}
     </View>
   );
 }
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,39 +1,61 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Keyboard, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native';
 import React, {useState} from 'react';
 
 import Card from '../components/Card';
 import Input from '../components/Input';
 import colors from '../constants/colors';
 
-const StartGameScreen= ()=> {
+const StartGameScreen= ({onStarGame})=> {
     const [enteredValue, setenteredValue] = useState('');
+    const [confirmedNumber, setconfirmedNumber] = useState(null)
+
     const handleInputValue= text =>{
         setenteredValue(text.replace(/[^0-9]/g,''));
     }
-    return(
-        <View style={styles.screen}>
-            <Text style={styles.title}>Comenzar Juego</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Elija un número</Text>
-                <Input
-                    maxLength={2}
-                    keyboardType="numeric"
-                    autoCapitalization="none"
-                    autoCorrect={false}
-                    value={enteredValue}
-                    onChangeText= {handleInputValue}
-                 />
-                <View style={styles.buttonContainer}>
-                    <Button title="Limpiar"  color= {colors.accent}/>
-                    <Button title="Confirmar" color= {colors.primary}/>
-                </View>
-            </Card>
-            <Card>
-                    <Text>Empezar el juego</Text>
-            </Card>
-        </View>
-    )
+    const handleResetInput = () => setenteredValue('');
+    const handleConfirmInput = () =>{
+        const choseNumber= parseInt(enteredValue)
+        if (choseNumber === NaN || choseNumber <= 0 || choseNumber > 99) return;
+        
+        setconfirmedNumber(enteredValue);
+        setenteredValue('');
+    }
 
+    const handleStartGame = () =>onStarGame(confirmedNumber)
+
+    const confirmedOuput = confirmedNumber ?(
+        <Card>
+            <Text>Numero seleccionado: {confirmedNumber}</Text>
+            <Button onPress={handleStartGame} title="EMPEZAR EL JUEGO" color= {colors.primary}/>
+        </Card>
+    ): null ;
+
+    return(
+        <TouchableWithoutFeedback onPress={()=> Keyboard.dismiss()}>
+
+            <View style={styles.screen}>
+                <Text style={styles.title}>Comenzar Juego</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Elija un número</Text>
+                    <Input
+                        maxLength={2}
+                        keyboardType="numeric"
+                        autoCapitalization="none"
+                        autoCorrect={false}
+                        value={enteredValue}
+                        onChangeText= {handleInputValue}
+                        />
+                    <View style={styles.buttonContainer}>
+                        <Button title="Limpiar"  color= {colors.accent} onPress={handleResetInput}/>
+                        <Button title="Confirmar" color= {colors.primary} onPress= {handleConfirmInput}/>
+                    </View>
+                </Card>
+                { confirmedOuput}
+                
+            </View>
+        </TouchableWithoutFeedback>
+    )
+    
 }
 
 const styles = StyleSheet.create({
